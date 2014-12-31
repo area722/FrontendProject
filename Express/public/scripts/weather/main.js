2efc36ce1f60198c6626fb93201182d50dcaa59b
area722 <wouter.waeytens@gmail.com>
2014-12-31 11:22:45 +0100

    weather styled

--- Express/public/scripts/weather/main.js
+++ Express/public/scripts/weather/main.js
@@ -23,7 +23,76 @@ function getWorker(){
 
 function workDone(res){
     console.log(res.data);
+    //location
+    $("#location").text(res.data.location.city+" - "+res.data.location.country);
 
+    //condition
+    getCondintions(res.data.item.forecast[0].code);
+    function getCondintions(con){
+        var img = $("#weather img")[0];
+        if(con >= 0 && con <= 47){
+            if(con >= 0 && con <= 4){
+                $(img).attr("src","/img/weather/lightning.svg");
+                return "thunder";
+            }
+            else if(con >= 5 && con <= 7){
+                $(img).attr("src","/img/weather/rain.svg");
+                return "rain";
+            }else if(con >= 8 && con <= 10){
+                $(img).attr("src","/img/weather/drizzle.svg");
+                return "drizzle";
+            }else if(con >= 11 && con <= 12){
+                $(img).attr("src","/img/weather/rain.svg");
+                return "rain";
+            }else if(con >= 13 && con <= 16){
+                $(img).attr("src","/img/weather/snow.svg");
+                return "snow";
+            }else if(con == 17){
+                $(img).attr("src","/img/weather/hail.svg");
+                return "hail";
+            }else if(con == 20){
+                $(img).attr("src","/img/weather/fog.svg");
+                return "fog";
+            }else if(con == 24){
+                $(img).attr("src","/img/weather/wind.svg");
+                return "wind";
+            }else if(con >= 27 && con <= 30){
+                $(img).attr("src","/img/weather/cloudsun.svg");
+                return "cloudsun";
+            }else if(con == 32){
+                $(img).attr("src","/img/weather/sun.svg");
+                return "sunny";
+            }else if(con >= 44 && con <= 47){
+                $(img).attr("src","/img/weather/cloudsun.svg");
+                return "cloudsun";
+            }
+            else{
+                $(img).attr("src","/img/weather/sun.svg");
+                return "sunny";
+            }
+        }
+        else{
+            console.log("there went something wrong");
+        }
+    }
+
+    //temp
+    getTemp(res.data.item.forecast[0].low,res.data.item.forecast[0].high);
+    function getTemp(low,high){
+        $("#tempHighP").text(high + "°");
+        $("#tempLowP").text(low + "°");
+
+        if(high >= 20){
+            $($("#tempHighImg")[0]).attr("src","/img/weather/thermometer100.svg")
+        }
+    }
+
+    //sun
+    getSun(res.data.astronomy.sunrise,res.data.astronomy.sunset);
+    function getSun(sunRise,sunSet){
+        $("#sunriseP").text(sunRise);
+        $("#sunsetP").text(sunSet);
+    }
 }
 
 function workErr(){
