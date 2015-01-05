bc671fa1497e5d05adbe3ee5c956aefb46078626
area722 <wouter.waeytens@gmail.com>
2015-01-05 15:57:49 +0100

    remove username

--- routes/routes.js
+++ routes/routes.js
@@ -6,7 +6,6 @@ module.exports = function(app){
     app.get("/home",function(req,res){
         var useragent = require('useragent');
         var agent = useragent.parse(req.headers['user-agent']);
-        var username = require('username');
         res.render("index",{title: "Mijn GsmTijdsLijn",time:"2004 - 2015",os:agent.os.toString()});
     });
 
